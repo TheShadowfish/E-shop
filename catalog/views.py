1029369dc4131f60cac206cf1afede351f6cd071
Эдуард Кусков <shadowfish@yandex.ru>
2024-05-02 22:18:51 +0300

    all tasks are implemented

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from datetime import datetime
+import os
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Write contacts: {name}({phone}): {message}')
+
+        parent_dir = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__))))
+        logger = Logger(os.path.join(parent_dir, 'data/contacts.txt'))
+        info = {'time': (datetime.now()).strftime('%Y-%m-%dT%H:%M:%S.%f'),
+                'name': name, 'phone': phone, 'message': message
+                }
+        logger(info)
+
+    return render(request, 'catalog/contacts.html')
+
+
+class Logger:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def __call__(self, info: dict):
+        # dict_user = {key: value for (key, value) in info.items()}
+        # dict_user['time'] = (datetime.now()).strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+        try:
+            with open(self.filepath, "at") as write_file:
+                write_file.write(str(info))
+                write_file.write('\n')
+        except OSError as e:
+            print(f"Something went wrong. I can't write \"{self.filepath}\", {str(e)}")
+
+
