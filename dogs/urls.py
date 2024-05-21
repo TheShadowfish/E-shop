f4a1441d54228092ca713cb0b1828b34151a095c
TheShadowfish <shadowfish@yandex.ru>
2024-05-21 11:26:07 +0300

    dogs in progress

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from dogs.apps import DogsConfig
-from dogs.views import dogs_list, contacts, dogs_detail
+from dogs.views import contacts, DogListView, DogDetailView, DogCreateView
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', dogs_list, name='dogs_list'),
-    path('dogs/<int:pk>/', dogs_detail, name='dogs_detail'),
+    path('', DogListView.as_view(), name='dogs_list'),
+    path('dogs/<int:pk>/', DogDetailView.as_view(), name='dogs_detail'),
     path('contacts/', contacts, name='contacts'),
+    path('dogs/create/', DogCreateView.as_view(), name='dogs_create')
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from dogs.models import Dog
 
+class DogListView(ListView):
+    model = Dog
 
-def dogs_list(request):
+class DogDetailView(DetailView):
+    model = Dog
 
-    dogs = Dog.objects.all()
-    context = {
-        "dogs": dogs,
-    }
-    return render(request, 'dogs/dogs_list.html', context)
+class DogCreateView(CreateView):
+    model= Dog
+    fields = ("name", "breed", "photo", "date_born", )
+    success_url = reverse_lazy('dogs:dog_list')
+# def dogs_list(request):
+#
+#     dogs = Dog.objects.all()
+#     context = {
+#         "dogs": dogs,
+#     }
+#     return render(request, 'dogs/dogs_list.html', context)
 
-
-def dogs_detail(request, pk):
-
-    # dog = Dog.objects.get(pk=pk)
-    dog = get_object_or_404(Dog, pk=pk)
-    context = {
-        "dog": dog,
-    }
-
-    return render(request, 'dogs/dogs_detail.html', context)
+#
+# def dogs_detail(request, pk):
+#
+#     # dog = Dog.objects.get(pk=pk)
+#     dog = get_object_or_404(Dog, pk=pk)
+#     context = {
+#         "dog": dog,
+#     }
+#
+#     return render(request, 'dogs/dogs_detail.html', context)
 
 
 def contacts(request):
