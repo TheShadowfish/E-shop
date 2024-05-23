1aa7d2a7f86eda49b6f86bd0feded040982c5751
TheShadowfish <shadowfish@yandex.ru>
2024-05-23 11:20:21 +0300

    счетчик просмотров реализован

--- article/views.py
+++ article/views.py
@@ -19,6 +19,11 @@ class ArticleListView(ListView):
 
 class ArticleDetailView(DetailView):
     model = Article
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class ArticleCreateView(CreateView):
