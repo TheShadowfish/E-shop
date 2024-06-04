61ed77ec43eead0c3c8bb31fccd6f03a2ac5d981
TheShadowfish <shadowfish@yandex.ru>
2024-06-04 09:38:43 +0300

    dogs_forms

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,16 +1,25 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 
 from dogs.models import Dog, Parent
 
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
 
-class DogForm(ModelForm):
+
+class DogForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Dog
         fields = "__all__"
         exclude = ("count_views",)
         # fields = ("name", "breed", "photo", "date_born",)
 
-class ParentForm(ModelForm):
+class ParentForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Parent
         fields = '__all__'
