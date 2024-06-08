91a75de8bf9c357c80420cab840175bbbcd521b9
TheShadowfish <shadowfish@yandex.ru>
2024-06-08 07:57:17 +0300

    3 задание in process +++++

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from django.forms import BooleanField
+
 from catalog.models import Product, Contact, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Product
@@ -28,7 +39,7 @@ class ProductForm(forms.ModelForm):
         else:
             return self.cleaned_data
 
-class ContactForm(forms.ModelForm):
+class ContactForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Contact
@@ -38,7 +49,7 @@ class ContactForm(forms.ModelForm):
             "message",
         )
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Version
