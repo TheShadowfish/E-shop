5839c4bc519f068bc3646aaee186b6bc27bd7235
TheShadowfish <shadowfish@yandex.ru>
2024-06-10 19:54:35 +0300

    auth: dogs 1

---
+++ d_users/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class D_user(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE, help_text='введите номер телефона')
+    tg_name = models.CharField(max_length=50, verbose_name='Ник телеграм', **NULLABLE, help_text='Введите ник телеграм')
+    avatar = models.ImageField(upload_to='d_users/avatars', verbose_name='Аватар', **NULLABLE, help_text='Загрузите свой аватар')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
