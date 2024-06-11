d2f806600dbcf880047cc73944b6cb1a3048c7b9
TheShadowfish <shadowfish@yandex.ru>
2024-06-11 19:58:35 +0300

    on start 22-2, регистрация по почте 2б редактирование профиля, запрет редактирования. ArticleForm. button=is_authenticated. For psw reset start. Нашел магию для сброса пароля, но как она работает с трудом понимаю. Все построенона имеющихся в django шаблонах.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView
+from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
 from users.views import RegisterView, ProfileView, email_verification, password_recovery
@@ -14,5 +14,14 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path("email-confirm/<str:token>/", email_verification, name='email-confirm'),
     path("password-recovery/", password_recovery, name='password_recovery'),
+
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html',
+                                                        email_template_name='users/password_reset_email.html',
+                                                      success_url=reverse_lazy('users:password_reset_done')),
+                                                        name='password_reset'),
+    path('password-reset/done/', PasswordResetView.as_view(template_name='users/password_reset_done.html'),
+                                                        name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                                                              success_url=reverse_lazy('users:login')), name='password_reset_confirm')
 
 ]
